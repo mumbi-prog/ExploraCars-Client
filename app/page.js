a64c7a41ba7d307786462defa24da297d356f1b4
Maureen <morynmoh@gmail.com>
2023-09-05 18:24:40 +0300

    home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import { LandingPage } from "@/components";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello from DHome </h1>
+      <h1>Hello from Home</h1>
+      <LandingPage />
       {/* <h2>start</h2> */}
     </>
   );
