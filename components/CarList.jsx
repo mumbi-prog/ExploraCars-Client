58803ca722f0905141522b08f4fdf1f9718d9e17
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-05 20:30:45 +0300

    Editing the car list title

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -8,14 +8,22 @@ const CarList = ({ cars }) => {
     <div className="car-list">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
+        viewBox="0 0 1440 220"
+        style={{ fontSize: '50px', marginBottom: '10px', paddingBottom: '5px' }} // Adjust marginBottom and paddingBottom
       >
         <path
           fill="#0099ff"
-          fill-opacity="0.7"
+          fillOpacity="0.7"
           d="M0,0L60,32C120,64,240,128,360,154.7C480,181,600,171,720,181.3C840,192,960,224,1080,197.3C1200,171,1320,85,1380,42.7L1440,0L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
         ></path>
-        <text x="50%" y="50%" fill="#fff" text-anchor="middle" dy="0.3em">
+        <text
+          x="50%"
+          y="40%" // Adjust vertical position
+          fill="#fff"
+          textAnchor="middle"
+          dy="0.3em"
+          style={{ textDecoration: 'underline' }}
+        >
           Our Automotive Inventory
         </text>
       </svg>
