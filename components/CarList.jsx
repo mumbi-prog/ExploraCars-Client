51c1b08b7d254ddcd2d5d181f55f9cedc94beba9
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-05 15:15:27 +0300

    Specified attributes to display

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const CarList = ({ cars }) => {
   return (
-    
+    <div className="car-list">
+      <h1>Car list</h1>
+      {cars.map((car) => (
+        <div key={car.id} className="car-card">
+          <div className='make-and bd-type'> <h2>{car.make}</h2> | <p> {car.body_type}</p></div>
+          <img src={car.image_url} alt={car.make} />
+          <p className='make-year'>Year: {car.year}</p>
+          <p className='price'>Price per Day: Ksh.{car.price_per_day}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
