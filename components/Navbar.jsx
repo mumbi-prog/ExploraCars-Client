f9c8b5a0689a6d05ba4a96c0ef55cd36f4138926
donvine mugendi <donvinemugendi@gmail.com>
2023-09-06 01:32:41 +0300

    updated navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,8 +13,8 @@ export default function Navbar() {
  
   return (
     <>
-      <div className="p-2 flex w-full items-center justify-between sticky top-1 dark:bg-slate-900 z-50 bg-gray-200">
-        <div className="">
+      <div className="p-2 flex w-full items-center justify-between sticky top-0 ">
+        <div className="w-10">
           {!menuOpen ? (
             <FaBars
               className="text-xl md:text-3xl z-50 cursor-pointer transition duration-150 ease-out"
@@ -29,14 +29,15 @@ export default function Navbar() {
           )}
         </div>
         
-        <div className="text-2xl text-center md:text-3xl font-bold lg:text-4xl cursor-pointer flex items-center xsm:text-xl m-auto">
+        <div className="text-2xl text-center md:text-3xl font-bold lg:text-4xl cursor-pointer flex items-center xsm:text-xl m-auto underline decoration-amber-400 decoration-4">
           <AiFillCar />
          <Link href="/"> Explora </Link>
         </div>
-            <button type="button" className="md:block btn-primary flex items-center cursor-pointer"><a href="/login"className="hover:text-blue-500 text-xl xsm:text-base"> 👤 Log In</a>
+            <button type="button" className="md:block btn-primary flex items-center cursor-pointer"><a href="/login"className="hover:text-black text-xl xsm:text-base"> Login</a>
             </button>
     
       </div>
+      <hr className="h-2 bg-amber-400"></hr>
     </>
   );
 }
