1b3cc4e0544fdeac928e61247824c8c1ce360ae4
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-05 16:44:40 +0300

    Added car grid container

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 const CarList = ({ cars }) => {
   return (
     <div className="car-list">
-      <h1>Car list</h1>
-      {cars.map((car) => (
-        <div key={car.id} className="car-card">
-          <div className='make-and bd-type'> <h2>{car.make}</h2> | <p> {car.body_type}</p></div>
-          <img src={car.image_url} alt={car.make} />
-          <p className='make-year'>Year: {car.year}</p>
-          <p className='price'>Price per Day: Ksh.{car.price_per_day}</p>
-        </div>
-      ))}
+      <h1>Our Automotive Inventory</h1>
+      <div className="card-grid-container">
+        {cars.map((car) => (
+          <div key={car.id} className="car-card">
+            <div className='make-and-bd-type'>
+              <h2>{car.make} |</h2>
+              <p> {car.body_type}</p>
+            </div>
+            <img src={car.image_url} alt={car.make} />
+            <p className='make-year'>Year: {car.year}</p>
+            <p className='price'>Price per Day: Ksh.{car.price_per_day}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
