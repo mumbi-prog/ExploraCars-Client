ef89d2bd76739cf4a47bc995d2966344f29d659a
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-05 23:42:11 +0300

    Editing displayed fleet

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -6,34 +6,39 @@ import { BsFuelPumpFill } from 'react-icons/bs';
 const CarList = ({ cars }) => {
   return (
     <div className="car-list">
-      <svg
+     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 220"
-        style={{ fontSize: '50px', marginBottom: '10px', paddingBottom: '5px' }} // Adjust marginBottom and paddingBottom
+        style={{ fontSize: '50px', marginBottom: '10px', paddingBottom: '5px' }}
       >
-        <path
-          fill="#0099ff"
-          fillOpacity="0.7"
-          d="M0,0L60,32C120,64,240,128,360,154.7C480,181,600,171,720,181.3C840,192,960,224,1080,197.3C1200,171,1320,85,1380,42.7L1440,0L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-        ></path>
+        <path 
+          fill="#3563e9" 
+          fill-opacity="0.7" 
+          d="M0,64L48,58.7C96,53,192,43,288,69.3C384,96,480,160,576,186.7C672,213,768,203,864,197.3C960,192,1056,192,1152,186.7C1248,181,1344,171,1392,165.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
+        </path>
         <text
           x="50%"
-          y="40%" // Adjust vertical position
+          y="45%"
           fill="#fff"
           textAnchor="middle"
           dy="0.3em"
-          style={{ textDecoration: 'underline' }}
+          style={{ textDecoration: 'underline', fontWeight: 'bold' }} 
         >
-          Our Automotive Inventory
+          Automotive Inventory
         </text>
       </svg>
+
       <div className="card-grid-container">
         {cars.map((car) => (
           <div key={car.id} className="car-card">
             <div className='make-and-bd-type'>
               <h2>{car.make}</h2>
             </div>
-            <p className='price'>Ksh.{car.price_per_day}/day</p>
+            <p className='price'>
+              <span className='superscript'>Ksh.</span>
+              {car.price_per_day}
+              <span className='subscript'>/day</span>
+            </p>
             <img src={car.image_url} alt={car.make} />
             
             <div className="car-details">
@@ -47,7 +52,7 @@ const CarList = ({ cars }) => {
             </div>
             <div className="icon-and-detail">
               <BsFuelPumpFill className="icon" />
-              <p>{car.fuel_type}</p>
+              <p>{car.fuel_consumption}L/km</p>
             </div>
           </div>
 
