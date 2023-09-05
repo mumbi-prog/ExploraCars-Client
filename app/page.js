f1c405999b69dff2faa492361f47e16182a3e5aa
Maureen <morynmoh@gmail.com>
2023-09-05 21:08:16 +0300

    CarSlider

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-
+import CarSlider from '../components/CarSlider'
 
 export default function Home() {
   return (
     <>
-      <h1>Hello from Home </h1>
+      <h1>Hello from Home  </h1>
+      <CarSlider />
     </>
   )
 }
