143b6a950a5c78cc6a068135124a8720de11fd99
donvine mugendi <donvinemugendi@gmail.com>
2023-09-06 23:36:36 +0300

    updated car slider

--- components/CarSlider.jsx
+++ components/CarSlider.jsx
@@ -65,9 +65,10 @@ function CarSlider() {
 
   return (
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+      <h1 className='font-bold text-3xl text-center my-2'>Discover Explora Car Rental Fleet</h1>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        className=' h-full rounded-2xl bg-center bg-cover duration-500'
       ></div>
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
