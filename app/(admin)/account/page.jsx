d3fedca1efa74365213dfa8998f1a79c19490145
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-09-06 20:57:32 +0300

    changing booking component name

--- app/(admin)/account/page.jsx
+++ app/(admin)/account/page.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { BookingList } from '@/components'
 
 export default function page() {
   return (
-    <div>page</div>
+    <>
+    <BookingList/>
+    </>
   )
 }
--- app/(admin)/booking/page.jsx
+++ app/(admin)/booking/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Booking } from '@/components'
 
 function page() {
   return (
-    <div>page</div>
+    <>
+    <Booking/>
+    </>
   )
 }
 
