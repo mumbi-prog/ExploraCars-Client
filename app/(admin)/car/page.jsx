bbb58c22449dfa64d69c93881a15c69713da314c
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-06 09:46:52 +0300

    Implemented car search functionality by make

--- app/(admin)/car/page.jsx
+++ app/(admin)/car/page.jsx
@@ -1,20 +1,24 @@
-"use client"
+'use client'
 import React, { useEffect, useState } from 'react';
-import { CarList } from '@/components';
+import { CarList, Search } from '@/components';
 
 function Page() {
   const [cars, setCars] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch('http://127.0.0.1:3000/cars')
       .then((response) => response.json())
-      .then((data) => setCars(data))
+      .then((data) => {
+        setCars(data);
+      })
       .catch((error) => console.error('Error fetching car data:', error));
   }, []);
 
   return (
     <>
-      <CarList cars={cars} />
+      <Search onSearchChange={setSearchQuery} />
+      <CarList cars={cars} searchQuery={searchQuery} />
     </>
   );
 }
