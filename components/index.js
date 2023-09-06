b1a1622d464f21aa6d8d038dabb888d24e3e86f4
donvine mugendi <donvinemugendi@gmail.com>
2023-09-06 21:05:59 +0300

    updated landing page

--- components/index.js
+++ components/index.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Booking from "./booking";
 import BookingList from "./BookingList.jsx";
+import Landing from "./Landing";
 
-export {Login, Register, Navbar, Footer, Booking, BookingList} 
+export {Login, Register, Navbar, Footer, Booking, BookingList, Landing} 
 
