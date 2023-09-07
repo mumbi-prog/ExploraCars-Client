d75c11f725e603bfcbdac32ba7518bed1b52c57e
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-09-07 15:06:50 +0300

    updating booking page

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 import Login from "./Login";
 import Register from "./Register"; 
-
+import Booking from "./Booking";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-export {Login, Register, Navbar, Footer} 
+import BookingList from "./BookingList";
+export {Login, Register, Navbar, Footer,Booking,BookingList} 
