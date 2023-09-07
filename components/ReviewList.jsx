b8376cffe5278364643f9fc92af467ad10e943be
Maureen <morynmoh@gmail.com>
2023-09-07 07:35:41 +0300

    ReviewList

---
+++ components/ReviewList.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+const ReviewList = ({ carId }) => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // Fetch reviews for a specific car when the component mounts
+    fetch(`/cars/${carId}/reviews`)
+      .then((response) => response.json())
+      .then((data) => setReviews(data));
+  }, [carId]);
+
+  return (
+    <div>
+      <h2>Reviews</h2>
+      <ul>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            <strong>{review.title}</strong> - Rating: {review.rating}
+            <p>{review.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ReviewList;
