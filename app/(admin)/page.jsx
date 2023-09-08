aa37d010f7fd1070d86bb393c8809a4d0253c7af
donvine mugendi <donvinemugendi@gmail.com>
2023-09-08 23:41:23 +0300

    updated page.js

--- app/(admin)/page.jsx
+++ app/(admin)/page.jsx
@@ -1,25 +1,26 @@
-'use client'
-import {useState, useEffect} from "react"
-import {Hero, CarList, Qualifications} from '@/components'
+"use client";
+import { useState, useEffect } from "react";
+import { Hero, CarList, Qualifications } from "@/components";
 export default function Home() {
   const [cars, setCars] = useState([]);
-  
 
   useEffect(() => {
-    fetch('http://127.0.0.1:3000/cars')
+    fetch("http://127.0.0.1:3000/cars")
       .then((response) => response.json())
       .then((data) => {
         setCars(data);
       })
-      .catch((error) => console.error('Error fetching car data:', error));
+      .catch((error) => console.error("Error fetching car data:", error));
   }, []);
   return (
     <>
       <Hero />
-      <h1 className='xsm:hero__subtitle hero__title text-center mx-2'>Explore our Rental Car Fleet</h1>
+      <h1 className="xsm:hero__subtitle hero__title text-center mx-2">
+        Explore our Rental Car Fleet
+      </h1>
       <CarList cars={cars} />
-      
-<Qualifications/>
+
+      <Qualifications />
     </>
-  )
+  );
 }
