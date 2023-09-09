c86bd0ef8eb70bc8fa26a6c7315cfdbae152818b
donvine mugendi <donvinemugendi@gmail.com>
2023-09-09 04:00:14 +0300

    updated accordion items

--- components/Qualifications.jsx
+++ components/Qualifications.jsx
@@ -5,16 +5,15 @@ export default function Qualifications() {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
-    <div className="mx-2 p-2 ">
+    <div className="p-2 ">
       <h1 className="lg:text-4xl text-3xl font-bold text-center xsm:text-xl my-2">Rental Terms, Qualifications and Requirements</h1>
-      <Accordion variant="shadow">
+      <Accordion variant="shadow"className="py-2">
         <AccordionItem
           key="1"
           aria-label="Driver requirements"
-          title="👤 Driver requirements"className="accordion">
-          <ol className="requirements-list mx-2 list-decimal">
-            <li>Minimum rental age is 18 years.</li>
-            <li>Maximum rental age is 65 years.</li>
+          title="👤 Driver requirements"className="accordion text-left">
+          <ol className="requirements-list mx-2">
+            <li>Minimum rental age is 18 years and the Maximum is 65.</li>
             <li>
               The driver license must have been issued by authorized authorities
               at least 1 year(s) before the date of the commencement of the
@@ -43,7 +42,7 @@ export default function Qualifications() {
             </li>
           </ol>
         </AccordionItem>
-        <AccordionItem key="2" aria-label="🛡️Protection" title="Accordion 2">
+        <AccordionItem key="2" aria-label="💳 Payment Policy" title="💳 Payment Policy"className="accordion">
           
         </AccordionItem>
         <AccordionItem key="3" aria-label="🛡️Protection" title="🛡️Protection"className="accordion">
@@ -51,6 +50,15 @@ export default function Qualifications() {
             <li>✔️ Collision Damage waiver
               <ul>
                 <li className="text-xs font-thin">You will have to pay at most the deductible if the cars bodywork is damaged (other parts of the car are not covered).</li>
+                </ul> 
+                </li>
+          </ul>
+        </AccordionItem>
+        <AccordionItem key="4" aria-label="📄 Rental Conditions " title="📄 Rental Conditions"className="accordion">
+          <ul className="requirements-list mx-2">
+            <li>✔️ Rate Includes
+              <ul>
+                <li className="text-xs font-thin">Free cancellation with a full refund up to 48 hours before your pick-up time, Unlimited mileage, Collision Damage Waiver, Roadside Assistance (in the country of pick-up), Third Party Liability and VAT (value added tax).</li>
                 </ul> 
                 </li>
           </ul>
