140aad86275244121321b222e2a1affc848e459c
donvine mugendi <donvinemugendi@gmail.com>
2023-09-10 21:34:53 +0300

    updated components.jsx

--- components/index.js
+++ components/index.js
@@ -4,7 +4,6 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Booking from "./Booking";
 import BookingList from "./BookingList.jsx";
-import Landing from "./Landing";
 import CarList from "./CarList";
 import Hero from "./Hero";
 import ReviewForm from './ReviewForm';
@@ -14,5 +13,5 @@ import Search from './Search';
 import Recomendations from "./Recomendations";
 
 
-export {Login, Register, Navbar, Footer, Booking, BookingList, Landing, CarList, Hero, ReviewForm, ReviewList, Qualifications, Search, Recomendations } 
+export {Login, Register, Navbar, Footer, Booking, BookingList, CarList, Hero, ReviewForm, ReviewList, Qualifications, Search, Recomendations } 
 
