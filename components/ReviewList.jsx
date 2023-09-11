c32007fda601464fae14760e5f6a72f3a6d457ba
donvine mugendi <donvinemugendi@gmail.com>
2023-09-11 03:06:00 +0300

    updated car reviews

--- components/ReviewList.jsx
+++ components/ReviewList.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const ReviewList = ({ carId }) => {
   const [reviews, setReviews] = useState([]);
-
+  const filteredReview = Array.isArray(reviews) ? reviews?.find((review) => review.car_id === carId) : null;
   useEffect(() => {
     fetch("https://explora-api.up.railway.app/reviews")
       .then((response) => response.json())
@@ -13,14 +13,16 @@ const ReviewList = ({ carId }) => {
 
   return (
     <div>
-      <h2 className='font-bold text-3xl text-center my-2'>Reviews</h2>
-      <ul>
-        {reviews && reviews.map((review) => (
-          <li key={review.id}>
-            <strong>{review.title}</strong> - Rating: {review.rating}
-            <p>{review.body}</p>
-          </li>
-        ))}
+      <h2 className="font-bold text-3xl text-center m-2 mx-auto">Reviews</h2>
+      <ul className="p-2 m-2 divide-y divide-blue-400 divide-dotted">
+        {filteredReview &&
+          filteredReview.map((review) => (
+            <li key={review.id}>
+              <strong className="font-bold ">{review.title}</strong>
+              <p className="text-base">{review.body}</p>
+              <p>Rating: {review.rating}</p>
+            </li>
+          ))}
       </ul>
     </div>
   );
