2c51352f18b1873b81bfa89b9ef247526cafa252
donvine mugendi <donvinemugendi@gmail.com>
2023-09-13 00:55:35 +0300

    updated data fetching for location

--- app/(admin)/locations/[name]/page.jsx
+++ app/(admin)/locations/[name]/page.jsx
@@ -1,33 +1,24 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 import { CarList, Search, Qualifications } from "@/components";
-import { getLocationCars } from "@/lib";
+// import { getLocationCars } from "@/lib";
 
 export default function Location({ params }) {
   const [cars, setCars] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const { replace } = useRouter();
   const validCities = ["nairobi", "eldoret", "mombasa"];
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const carData = await getLocationCars(params.name);
-        setCars(carData);
-      } catch (error) {
-        console.error("Error fetching car data:", error);
-        replace("/cars");
-      }
-    }
-
-    fetchData();
-  }, [params.name, replace]);
   useEffect(() => {
     if (!validCities.includes(params.name)) {
       replace("/not-found");
     }
   }, [replace, validCities, params.name]);
+  useEffect(() => {
+    fetch(`https://explora-api.up.railway.app/locations/${params.name}`)
+      .then((res) => res.json())
+      .then((data) => setCars(data));
+  }, [params.name, replace]);
 
   const filteredCars = cars?.filter(
     (car) =>
@@ -38,7 +29,8 @@ export default function Location({ params }) {
   return (
     <>
       <h1 className="font-bold text-3xl xsm:text-2xl text-center my-2">
-        Explora {params.name} car rental
+        Explora {validCities.includes(params.name) ? params.name : ""} car
+        rental
       </h1>
       <Search setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
       <CarList cars={filteredCars} itemsPerPage={12} Search={Search} />
